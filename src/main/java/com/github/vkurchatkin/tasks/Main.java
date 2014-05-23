5fc7db95a12dfa727a775fcbb8f8bfc90e67e3bb
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2014-05-23 15:43:56 +0400

    bootstrapping from resource

--- src/main/java/com/github/vkurchatkin/tasks/Main.java
+++ src/main/java/com/github/vkurchatkin/tasks/Main.java
@@ -1,5 +1,11 @@
 package com.github.vkurchatkin.tasks;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 /**
  * User: vk
  * Date: 23/05/14
@@ -7,6 +13,16 @@ package com.github.vkurchatkin.tasks;
  */
 public class Main {
     public static void main(String [] args) {
-        System.out.println("Hello world");
+        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+
+        try {
+            InputStream stream = Main.class.getClassLoader().getResourceAsStream("bootstrap.js");
+            Reader reader = new InputStreamReader(stream);
+
+            engine.eval(reader);
+        } catch (Exception e) {
+            System.err.println("Initialization failed");
+            System.exit(-1);
+        }
     }
 }
