85d838da087e991e2bf09cb9015abf78b68e0ce1
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2014-05-23 16:35:19 +0400

    fix error reporting

--- src/main/java/com/github/vkurchatkin/tasks/runtime/Runtime.java
+++ src/main/java/com/github/vkurchatkin/tasks/runtime/Runtime.java
@@ -4,7 +4,9 @@ import jdk.nashorn.api.scripting.NashornScriptEngine;
 import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import jdk.nashorn.api.scripting.ScriptObjectMirror;
 
+import javax.script.ScriptEngine;
 import javax.script.ScriptException;
+import javax.script.SimpleBindings;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -28,16 +30,25 @@ public class Runtime {
     }
 
     synchronized public void run () throws RuntimeException {
-        InputStream stream = this.getClass().getClassLoader().getResourceAsStream("bootstrap.js");
-        Reader reader = new InputStreamReader(stream);
 
         ScriptObjectMirror res = null;
         try {
-            res = (ScriptObjectMirror) engine.eval(reader);
+            res = (ScriptObjectMirror) runInternal("bootstrap.js");
         } catch (ScriptException e) {
             throw new RuntimeException(e);
         }
-        res.call(null, bindings);
 
+        res.call(null, bindings);
+    }
+
+    private Object runInternal (String filename) throws ScriptException {
+        InputStream stream = this.getClass().getClassLoader().getResourceAsStream("bootstrap.js");
+        Reader reader = new InputStreamReader(stream);
+
+        SimpleBindings b = new SimpleBindings();
+
+        b.put(ScriptEngine.FILENAME, filename);
+
+        return engine.eval(reader, b);
     }
 }
--- src/main/resources/bootstrap.js
+++ src/main/resources/bootstrap.js
@@ -1,6 +1,28 @@
-(function (bindings) {
+(function bootstrap (bindings) {
   var global = this;
 
-  bindings.log(Object.getOwnPropertyNames(global).join(', '));
+  var console = {
+    log : function (str) {
+      bindings.log(str);
+    }
+  };
+
+  function test () {
+    outer();
+  }
+
+  function bad () {
+    test();
+  }
+
+  try {
+    bad();
+  } catch (e) {
+    console.log(e.stack)
+  }
 
 });
+
+function outer () {
+  throw new Error('test')
+}
