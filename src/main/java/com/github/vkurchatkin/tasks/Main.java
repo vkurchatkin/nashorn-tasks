2dd4cff7db4ea935c21cf4d3163d2cbeee4ceb25
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2014-05-23 15:28:35 +0400

    Initial commit

---
+++ src/main/java/com/github/vkurchatkin/tasks/Main.java
@@ -0,0 +1,12 @@
+package com.github.vkurchatkin.tasks;
+
+/**
+ * User: vk
+ * Date: 23/05/14
+ * Time: 15:09
+ */
+public class Main {
+    public static void main(String [] args) {
+        System.out.println("Hello world");
+    }
+}
